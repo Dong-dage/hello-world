ba811936e4e2f8abf8b5eadb4e0db3df55c86dfd
Sean <qiganpingfan@aliyun.com>
2017-10-24 21:07:31 +0800

    Create UserBean.java

---
+++ src/com/want/wx/pojo/UserBean.java
@@ -0,0 +1,26 @@
+package com.want.wx.pojo;
+
+public class UserBean {
+	private String userid;
+	private String name;
+	private String[] department;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getDepartment() {
+		return department;
+	}
+	public void setDepartment(String[] department) {
+		this.department = department;
+	}
+	
+}
