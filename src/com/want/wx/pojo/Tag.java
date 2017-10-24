04bf3296c3238a8c1ec866435f57d1e9f0ba46a6
Sean <qiganpingfan@aliyun.com>
2017-10-24 21:08:35 +0800

    Create Tag.java

---
+++ src/com/want/wx/pojo/Tag.java
@@ -0,0 +1,26 @@
+package com.want.wx.pojo;
+
+public class Tag {
+	private String wechat_tagid;
+	private String hr_tagid;
+	private String tagname;
+	public String getWechat_tagid() {
+		return wechat_tagid;
+	}
+	public void setWechat_tagid(String wechat_tagid) {
+		this.wechat_tagid = wechat_tagid;
+	}
+	public String getHr_tagid() {
+		return hr_tagid;
+	}
+	public void setHr_tagid(String hr_tagid) {
+		this.hr_tagid = hr_tagid;
+	}
+	public String getTagname() {
+		return tagname;
+	}
+	public void setTagname(String tagname) {
+		this.tagname = tagname;
+	}
+	
+}
