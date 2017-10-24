acee7285ed55c017301ff765eb29ad3587ea5512
Sean <qiganpingfan@aliyun.com>
2017-10-24 21:09:06 +0800

    Create Emp.java

---
+++ src/com/want/wx/pojo/Emp.java
@@ -0,0 +1,76 @@
+package com.want.wx.pojo;
+
+public class Emp {
+	//分公司、营业所、工号、姓名、手机号、email、职务、性别、部门、关注状态
+	private String companyName;
+	private String branchName;
+	private String uName;
+	private String uId;
+	private String position;
+	private String mobile;
+	private String email;
+	private String gender;
+	private String dId;
+	private String status;
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}	
+	public String getuId() {
+		return uId;
+	}
+	public void setuId(String uId) {
+		this.uId = uId;
+	}
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getdId() {
+		return dId;
+	}
+	public void setdId(String dId) {
+		this.dId = dId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
